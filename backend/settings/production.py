ab6fefc9be0c57cb7161ac8c47783092221652b1
Jeffrey A. Clark <aclark@aclark.net>
2024-12-08 18:53:42 -0500

    Get hostname

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -2,7 +2,7 @@ import dj_database_url
 import os
 
 from .base import *  # noqa: F403
-from backend.utils import get_ec2_metadata
+from backend.utils import get_ec2_hostname
 
 DEBUG = False
 
@@ -15,7 +15,7 @@ MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware")  # noqa
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-LOCAL_IPV4 = get_ec2_metadata()
+LOCAL_IPV4 = get_ec2_hostname()
 ALLOWED_HOSTS.append(LOCAL_IPV4)  # noqa
 
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
--- backend/utils.py
+++ backend/utils.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_ec2_metadata():
+def get_ec2_ip():
     try:
         # Step 1: Get the token
         token_url = "http://169.254.169.254/latest/api/token"
@@ -21,4 +21,27 @@ def get_ec2_metadata():
         return metadata
     except requests.RequestException as e:
         print(f"Error retrieving EC2 metadata: {e}")
+        return None
+
+
+def get_ec2_hostname():
+    try:
+        # Step 1: Get the token
+        token_url = "http://169.254.169.254/latest/api/token"
+        headers = {"X-aws-ec2-metadata-token-ttl-seconds": "21600"}
+        response = requests.put(token_url, headers=headers)
+        response.raise_for_status()  # Raise an error for bad responses
+
+        token = response.text
+
+        # Step 2: Use the token to get the instance hostname
+        hostname_url = "http://169.254.169.254/latest/meta-data/hostname"
+        headers = {"X-aws-ec2-metadata-token": token}
+        response = requests.get(hostname_url, headers=headers)
+        response.raise_for_status()  # Raise an error for bad responses
+
+        hostname = response.text
+        return hostname
+    except requests.RequestException as e:
+        print(f"Error retrieving EC2 hostname: {e}")
         return None
