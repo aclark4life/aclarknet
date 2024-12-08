8a1ffdf95b58ebdc9cc513683024e88b931b87c1
Jeffrey A. Clark <aclark@aclark.net>
2024-12-08 17:27:12 -0500

    Add allowed hosts

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,6 +1,7 @@
 import os
 
 from .base import *  # noqa: F403
+from backend.utils import get_ec2_metadata
 
 DEBUG = False
 
@@ -12,3 +13,6 @@ except ImportError:
 MIDDLEWARE.append("whitenoise.middleware.WhiteNoiseMiddleware")  # noqa
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+LOCAL_IPV4 = get_ec2_metadata()
+ALLOWED_HOSTS.append(LOCAL_IPV4)  # noqa
