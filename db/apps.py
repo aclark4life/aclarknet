e3c5dc882a9101a3d51098e6b8e80a448e629085
Jeffrey A. Clark <aclark@aclark.net>
2025-02-15 17:26:32 -0500

    Set default auto field

--- db/apps.py
+++ db/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class DBConfig(AppConfig):
     name = "db"
+    default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
 
     def ready(self):
         import db.signals  # noqa
