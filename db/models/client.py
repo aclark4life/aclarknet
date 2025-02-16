e7f8adc67bc937afda7b8ba0c09b6d22cf9d7891
Jeffrey A. Clark <aclark@aclark.net>
2025-02-16 16:58:03 -0500

    Remove taggit field

--- db/models/client.py
+++ db/models/client.py
@@ -1,12 +1,12 @@
 from django.db import models
-from taggit.managers import TaggableManager
+# from taggit.managers import TaggableManager
 
 from db.models.base import BaseModel
 from django.shortcuts import reverse
 
 
 class Client(BaseModel):
-    tags = TaggableManager(blank=True)
+    # tags = TaggableManager(blank=True)
     address = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     url = models.URLField("Website", blank=True, null=True)
