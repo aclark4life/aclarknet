6cc8f0b60cc880108bea5093945b0fb56d80b98c
Jeffrey A. Clark <aclark@aclark.net>
2025-03-02 15:32:43 -0500

    Fix react imports

--- frontend/src/components/UserMenu.js
+++ frontend/src/components/UserMenu.js
@@ -1,5 +1,5 @@
 // UserMenu.js
-import React from 'react';
+import React from 'react';  // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
 
 function handleLogout() {  // eslint-disable-line no-unused-vars
