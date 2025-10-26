ee5200ad7dc27aa5902d6099278cef6105094bba
Jeffrey A. Clark <aclark@aclark.net>
2025-10-26 16:18:57 -0400

    Update company admin

--- db/admin.py
+++ db/admin.py
@@ -1,6 +1,20 @@
+from django import forms
 from django.contrib import admin
 from .models import Company
 
+
+class CompanyAdminForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = "__all__"
+        widgets = {
+            "website": forms.URLInput(attrs={"size": 60}),
+        }
+
+
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    pass
+    # Automatically include all model fields in the list display
+
+    form = CompanyAdminForm
+    list_display = [field.name for field in Company._meta.fields]
