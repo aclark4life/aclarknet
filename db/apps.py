3803ce43034690f7e7d204e6fb27ab61a8e265d7
Jeffrey A. Clark <aclark@aclark.net>
2025-11-06 21:34:46 -0500

    Set verbose_name

--- db/apps.py
+++ db/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class DbConfig(AppConfig):
-    default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
-    name = 'db'
+    default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
+    name = "db"
+    verbose_name = "DB"
