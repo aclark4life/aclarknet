7151d1c191eeaa509a9a828eb6d07f85c065d308
Jeffrey A. Clark <aclark@aclark.net>
2025-12-25 15:07:27 -0500

    Update settings.py

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'aclarknet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_mongodb_backend',
+        'NAME': 'aclarknet',
+        'HOST': os.environ.get('MONGODB_URI', 'localhost'),
     }
 }
 
