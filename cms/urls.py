584b3ccc3e2bd53efa9f6cc873620d562dfed5dc
Jeffrey A. Clark <aclark@aclark.net>
2025-12-30 22:01:42 -0500

    Add services

--- cms/urls.py
+++ cms/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ServicesView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("services", ServicesView.as_view(), name="services"),
 ]
--- cms/views.py
+++ cms/views.py
@@ -10,3 +10,17 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["message"] = "Welcome to the Home Page!"
         return context
+
+
+class ServicesView(TemplateView):
+    template_name = "services.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["services"] = [
+            "Web Development",
+            "App Development",
+            "SEO Optimization",
+            "Digital Marketing",
+        ]
+        return context
